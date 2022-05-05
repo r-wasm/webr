34f5eace4741647011a2b519179f46dc718ba681
Lionel Henry <lionel.hry@gmail.com>
2022-05-05 10:09:55 +0200

    Rename `_LDFLAG` suffix to conventional `_LDADD`

--- tools/fortran-dragonegg.mk
+++ tools/fortran-dragonegg.mk
@@ -2,4 +2,4 @@ EMFC_DIR = $(WEBR_ROOT)/tools/dragonegg
 EMFC = $(EMFC_DIR)/emfc
 
 FORTRAN_WASM_LIB = $(WASM)/lib/libgfortran.a
-FORTRAN_WASM_LDFLAG = -L$(WASM)/lib -lgfortran
+FORTRAN_WASM_LDADD = -L$(WASM)/lib -lgfortran
--- tools/fortran-flang.mk
+++ tools/fortran-flang.mk
@@ -2,4 +2,4 @@ EMFC_DIR = $(WEBR_ROOT)/tools/flang
 EMFC = $(EMFC_DIR)/emfc
 
 FORTRAN_WASM_LIB = $(WASM)/lib/libFortranRuntime.a
-FORTRAN_WASM_LDFLAG = -L$(WASM)/lib -lFortranRuntime
+FORTRAN_WASM_LDADD = -L$(WASM)/lib -lFortranRuntime
