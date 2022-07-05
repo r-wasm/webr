d9a852ada3a9784dbc9fbb465fd8a6ce70557708
George Stagg <george.stagg@rstudio.com>
2022-07-05 10:42:56 +0100

    Add some more tests for webR
    Passing package tests now included:
     * datasets
     * graphics
     * stats4
    
    A small subset of the tests from the base package tests has been
    manually copied to the test file base.R. Once futher development has
    reached the point where the original base package tests pass we can
    replace base.R with running the original test package.

---
+++ tests/base.R
@@ -0,0 +1,147 @@
+x <- -1:12
+x + 1
+2 * x + 3
+x %% 2 #-- is periodic
+x %/% 5
+x %% Inf # now is defined by limit (gave NaN in earlier versions of R)
+1:4
+pi:6 # real
+6:pi # integer
+f1 <- gl(2, 3); f1
+f2 <- gl(3, 2); f2
+f1:f2 # a factor, the "cross"  f1 x f2
+x <- stats::rnorm(20)
+x < 1
+x[x > 0]
+x1 <- 0.5 - 0.3
+x2 <- 0.3 - 0.1
+x1 == x2                   # FALSE on most machines
+isTRUE(all.equal(x1, x2))  # TRUE everywhere
+for(i in 1:5) print(1:i)
+for(n in c(2,5,10,20,50)) {
+   x <- stats::rnorm(n)
+   cat(n, ": ", sum(x^2), "\n", sep = "")
+}
+f <- factor(sample(letters[1:5], 10, replace = TRUE))
+for(i in unique(f)) print(i)
+y <- 1 + (x <- stats::rpois(50, lambda = 1.5) / 4 - 1)
+x[(x > 0) & (x < 1)]    # all x values between 0 and 1
+if (any(x == 0) || any(y == 0)) "zero encountered"
+x <- c(NA, FALSE, TRUE)
+names(x) <- as.character(x)
+outer(x, x, "&") ## AND table
+outer(x, x, "|") ## OR  table
+is.na(c(1, NA))        #> FALSE  TRUE
+is.na(paste(c(1, NA))) #> FALSE FALSE
+(xx <- c(0:4))
+is.na(xx) <- c(2, 4)
+xx                     #> 0 NA  2 NA  4
+anyNA(xx) # TRUE
+# Some logical operations do not return NA
+c(TRUE, FALSE) & NA
+c(TRUE, FALSE) | NA
+## anyNA() can work recursively with list()s:
+LL <- list(1:5, c(NA, 5:8), c("A","NA"), c("a", NA_character_))
+L2 <- LL[c(1,3)]
+sapply(LL, anyNA); c(anyNA(LL), anyNA(LL, TRUE))
+sapply(L2, anyNA); c(anyNA(L2), anyNA(L2, TRUE))
+## ... lists, and hence data frames, too:
+dN <- dd <- USJudgeRatings; dN[3,6] <- NA
+anyNA(dd) # FALSE
+anyNA(dN) # TRUE
+is.null(list())     # FALSE (on purpose!)
+is.null(pairlist()) # TRUE
+is.null(integer(0)) # FALSE
+is.null(logical(0)) # FALSE
+as.null(list(a = 1, b = "c"))
+## You can create numbers using fixed or scientific formatting.
+2.1
+2.1e10
+-2.1E-10
+## The resulting objects have class numeric and type double.
+class(2.1)
+typeof(2.1)
+## This holds even if what you typed looked like an integer.
+class(2)
+typeof(2)
+## If you actually wanted integers, use an "L" suffix.
+class(2L)
+typeof(2L)
+## These are equal but not identical
+2 == 2L
+identical(2, 2L)
+## You can write numbers between 0 and 1 without a leading "0"
+## (but typically this makes code harder to read)
+.1234
+sqrt(1i) # remember elementary math?
+utils::str(0xA0)
+identical(1L, as.integer(1))
+## You can combine the "0x" prefix with the "L" suffix :
+identical(0xFL, as.integer(15))
+f <- get("(")
+e <- expression(3 + 2 * 4)
+identical(f(e), e)
+do <- get("{")
+do(x <- 3, y <- 2*x-3, 6-x-y); x; y
+## note the differences
+(2+3)
+{2+3; 4+5}
+(invisible(2+3))
+{invisible(2+3)}
+## A trivial (but inefficient!) example:
+fib <- function(n)
+   if(n<=2) { if(n>=0) 1 else 0 } else Recall(n-1) + Recall(n-2)
+fibonacci <- fib; rm(fib)
+## renaming wouldn't work without Recall
+fibonacci(10) # 55
+round(.5 + -2:4) # IEEE / IEC rounding: -2  0  0  2  2  4  4
+( x1 <- seq(-2, 4, by = .5) )
+round(x1) #-- IEEE / IEC rounding !
+x1[trunc(x1) != floor(x1)]
+x1[round(x1) != floor(x1 + .5)]
+(non.int <- ceiling(x1) != floor(x1))
+x2 <- pi * 100^(-1:3)
+round(x2, 3)
+signif(x2, 3)
+## Logical AND ("&&") has higher precedence than OR ("||"):
+TRUE || TRUE && FALSE   # is the same as
+TRUE || (TRUE && FALSE) # and different from
+(TRUE || TRUE) && FALSE
+## Special operators have higher precedence than "!" (logical NOT).
+## You can use this for %in% :
+! 1:10 %in% c(2, 3, 5, 7) # same as !(1:10 %in% c(2, 3, 5, 7))
+## but we strongly advise to use the "!( ... )" form in this case!
+x <- seq(-3, 7, by = 1/8)
+tx <- cbind(x, cos(pi*x), cospi(x), sin(pi*x), sinpi(x),
+               tan(pi*x), tanpi(x), deparse.level=2)
+op <- options(digits = 4, width = 90) # for nice formatting
+head(tx)
+tx[ (x %% 1) %in% c(0, 0.5) ,]
+options(op)
+# We use rep.int as rep is primitive
+vrep <- Vectorize(rep.int)
+vrep(1:4, 4:1)
+vrep(times = 1:4, x = 4:1)
+vrep <- Vectorize(rep.int, "times")
+vrep(times = 1:4, x = 42)
+f <- function(x = 1:3, y) c(x, y)
+vf <- Vectorize(f, SIMPLIFY = FALSE)
+f(1:3, 1:3)
+vf(1:3, 1:3)
+vf(y = 1:3) # Only vectorizes y, not x
+# Nonlinear regression contour plot, based on nls() example
+require(graphics)
+SS <- function(Vm, K, resp, conc) {
+    pred <- (Vm * conc)/(K + conc)
+    sum((resp - pred)^2 / pred)
+}
+vSS <- Vectorize(SS, c("Vm", "K"))
+Treated <- subset(Puromycin, state == "treated")
+Vm <- seq(140, 310, length.out = 50)
+K <- seq(0, 0.15, length.out = 40)
+SSvals <- outer(Vm, K, vSS, Treated$rate, Treated$conc)
+contour(Vm, K, SSvals, levels = (1:10)^2, xlab = "Vm", ylab = "K")
+# combn() has an argument named FUN
+combnV <- Vectorize(function(x, m, FUNV = NULL) combn(x, m, FUN = FUNV),
+                    vectorize.args = c("x", "m"))
+combnV(4, 1:4)
