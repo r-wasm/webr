f78a97ef692e7b81152dfded147d38e128249746
George Stagg <george.stagg@rstudio.com>
2022-08-24 16:00:11 +0100

    Add explanatory comment for the XHR worker fetch

--- src/webR/chan/channel.ts
+++ src/webR/chan/channel.ts
@@ -43,6 +43,23 @@ export class ChannelMain {
     };
 
     if (url.startsWith('http')) {
+      /* Workaround for loading a cross-origin script.
+       *
+       * When fetching a worker script, the fetch is required by the spec to
+       * use "same-origin" mode. This is to avoid loading a worker with a
+       * cross-origin global scope, which can allow for a cross-origin
+       * restriction bypass.
+       *
+       * When the fetch URL begins with 'http', we assume the request is
+       * cross-origin. We download the content of the URL using a XHR first,
+       * create a blob URL containing the requested content, then load the
+       * blob URL as a script.
+       *
+       * The origin of a blob URL is the same as that of the environment that
+       * created the URL, and so the global scope of the resulting worker is
+       * no longer cross-origin. In that case, the cross-origin restriction
+       * bypass is not possible, and the script is permitted to be loaded.
+       */
       const req = new XMLHttpRequest();
       req.open('get', url, true);
       req.onload = () => {
