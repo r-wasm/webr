e1f2a858505f978e2eb6250983d1688edcabcf84
George Stagg <george.stagg@rstudio.com>
2022-09-01 08:27:54 +0100

    Minor changes to webr package
    Co-authored-by: Lionel Henry <lionel.hry@gmail.com>

--- packages/webr/R/handlers.R
+++ packages/webr/R/handlers.R
@@ -1,13 +1,13 @@
 #' Prompt user to install missing packages from a webR binary repo
 #'
 #' @description
-#' When enabled, `packageNotFoundError` thrown causes a menu prompt
+#' When enabled, `packageNotFoundError` errors cause a menu prompt
 #' to be shown to the user asking if they would like to attempt to
 #' download the missing package.
 #' 
 #' If the user replies in the negative, the package error is thrown as
 #' it would normally. If the user replies in the affirmative, the package
-#' is downloaded using `webr::install()`.
+#' is downloaded using [webr::install()].
 #' 
 #' If it is possible to do so, execution will continue without
 #' interrupting the current program.
@@ -16,14 +16,16 @@
 global_prompt_install <- function() {
   globalCallingHandlers(
     packageNotFoundError = function(cnd) {
-      if (!interactive()) return()
+      if (!interactive()) {
+        return()
+      }
       pkg <- cnd$package
       download <- utils::menu(
         c("Yes", "No"),
-        title=paste(
+        title = paste(
           'Failed to load package "', pkg,
           '". Do you want to try downloading it from the webR binary repo?',
-          sep=''
+          sep = ""
         )
       )
       if (download == 1) {
