4bc4cc7e7382da88c5dbfd7b03fddc8f9a34d779
George Stagg <george.stagg@rstudio.com>
2022-10-05 14:40:16 +0100

    Update webR default package unit tests
    Do not attempt to capture the output streams or any
    conditions thrown by the webr::test_package tests.

--- src/tests/packages/webr.test.ts
+++ src/tests/packages/webr.test.ts
@@ -7,6 +7,13 @@ const webR = new WebR({
   interactive: false,
 });
 
+function evalTest(pkg: string) {
+  return webR.evalRCode(`webr::test_package("${pkg}")`, undefined, {
+    captureConditions: false,
+    captureStreams: false,
+  });
+}
+
 beforeAll(async () => {
   await webR.init();
   // Install packages required to run all package examples and tests
@@ -20,63 +27,63 @@ test('The webr R package is installed', async () => {
 
 describe('Run R default package examples and tests', () => {
   test('datasets', async () => {
-    const test = (await webR.evalRCode('webr::test_package("datasets")')).result as RDouble;
+    const test = (await evalTest('datasets')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('tools', async () => {
-    const test = (await webR.evalRCode('webr::test_package("tools")')).result as RDouble;
+    const test = (await evalTest('tools')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('utils', async () => {
-    const test = (await webR.evalRCode('webr::test_package("utils")')).result as RDouble;
+    const test = (await evalTest('utils')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('grDevices', async () => {
-    const test = (await webR.evalRCode('webr::test_package("grDevices")')).result as RDouble;
+    const test = (await evalTest('grDevices')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('graphics', async () => {
-    const test = (await webR.evalRCode('webr::test_package("graphics")')).result as RDouble;
+    const test = (await evalTest('graphics')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('stats', async () => {
-    const test = (await webR.evalRCode('webr::test_package("stats")')).result as RDouble;
+    const test = (await evalTest('stats')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('stats4', async () => {
-    const test = (await webR.evalRCode('webr::test_package("stats4")')).result as RDouble;
+    const test = (await evalTest('stats4')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('splines', async () => {
-    const test = (await webR.evalRCode('webr::test_package("splines")')).result as RDouble;
+    const test = (await evalTest('splines')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('methods', async () => {
-    const test = (await webR.evalRCode('webr::test_package("methods")')).result as RDouble;
+    const test = (await evalTest('methods')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('compiler', async () => {
-    const test = (await webR.evalRCode('webr::test_package("compiler")')).result as RDouble;
+    const test = (await evalTest('compiler')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('base', async () => {
-    const test = (await webR.evalRCode('webr::test_package("base")')).result as RDouble;
+    const test = (await evalTest('base')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 
   test('grid', async () => {
     await webR.evalRCode('options(expressions=5000)');
-    const test = (await webR.evalRCode('webr::test_package("grid")')).result as RDouble;
+    const test = (await evalTest('grid')).result as RDouble;
     expect(await test.toNumber()).toEqual(0);
   });
 });
