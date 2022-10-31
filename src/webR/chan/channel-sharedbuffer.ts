9f1cf0da0dcf4699cae1bbb1ee71c120f0313816
George Stagg <george.stagg@rstudio.com>
2022-10-31 11:54:18 +0000

    Make ChannelMain/ChannelWorker abstract interfaces

--- src/webR/chan/channel-sharedbuffer.ts
+++ src/webR/chan/channel-sharedbuffer.ts
@@ -1,14 +1,7 @@
-import { AsyncQueue } from './queue';
-import { promiseHandles, ResolveFn } from '../utils';
-import { Message, newRequest, Response, SyncRequest } from './message';
-import { Endpoint } from './task-common';
-import { syncResponse } from './task-main';
-
+import { Message } from './message';
+import { SharedBufferChannelMain, SharedBufferChannelWorker } from './channel-sab';
+import { WebROptions } from '../webr-main';
 import { IN_NODE } from '../compat';
-import type { Worker as NodeWorker } from 'worker_threads';
-if (IN_NODE) {
-  (globalThis as any).Worker = require('worker_threads').Worker as NodeWorker;
-}
 
 // The channel structure is asymetric:
 //
@@ -30,182 +23,45 @@ if (IN_NODE) {
 //   serialised. There is no structured cloning involved, and
 //   ArrayBuffers can't be transferred, only copied.
 
-// Main ----------------------------------------------------------------
-
-export class ChannelMain {
-  inputQueue = new AsyncQueue<Message>();
-  outputQueue = new AsyncQueue<Message>();
-  #interruptBuffer?: Int32Array;
-
+export interface ChannelMain {
   initialised: Promise<unknown>;
-  resolve: (_?: unknown) => void;
-  close = () => {};
-
-  #parked = new Map<string, ResolveFn>();
-
-  constructor(url: string, data: unknown) {
-    const initWorker = (worker: Worker) => {
-      this.#handleEventsFromWorker(worker);
-      this.close = () => worker.terminate();
-      const msg = { type: 'init', data: data } as Message;
-      worker.postMessage(msg);
-    };
-
-    if (url.startsWith('http')) {
-      /* Workaround for loading a cross-origin script.
-       *
-       * When fetching a worker script, the fetch is required by the spec to
-       * use "same-origin" mode. This is to avoid loading a worker with a
-       * cross-origin global scope, which can allow for a cross-origin
-       * restriction bypass.
-       *
-       * When the fetch URL begins with 'http', we assume the request is
-       * cross-origin. We download the content of the URL using a XHR first,
-       * create a blob URL containing the requested content, then load the
-       * blob URL as a script.
-       *
-       * The origin of a blob URL is the same as that of the environment that
-       * created the URL, and so the global scope of the resulting worker is
-       * no longer cross-origin. In that case, the cross-origin restriction
-       * bypass is not possible, and the script is permitted to be loaded.
-       */
-      const req = new XMLHttpRequest();
-      req.open('get', url, true);
-      req.onload = () => {
-        const worker = new Worker(URL.createObjectURL(new Blob([req.responseText])));
-        initWorker(worker);
-      };
-      req.send();
-    } else {
-      const worker = new Worker(url);
-      initWorker(worker);
-    }
-
-    ({ resolve: this.resolve, promise: this.initialised } = promiseHandles());
-  }
-
-  async read() {
-    return await this.outputQueue.get();
-  }
-
-  async flush() {
-    const msg: Message[] = [];
-    while (!this.outputQueue.isEmpty()) {
-      msg.push(await this.read());
-    }
-    return msg;
-  }
-
-  interrupt() {
-    if (!this.#interruptBuffer) {
-      throw new Error('Failed attempt to interrupt before initialising interruptBuffer');
-    }
-    this.#interruptBuffer[0] = 1;
-  }
-
-  write(msg: Message) {
-    this.inputQueue.put(msg);
-  }
-
-  async request(msg: Message, transferables?: [Transferable]): Promise<any> {
-    const req = newRequest(msg, transferables);
-
-    const { resolve: resolve, promise: prom } = promiseHandles();
-    this.#parked.set(req.data.uuid, resolve);
-
-    this.write(req);
-    return prom;
-  }
-
-  #resolveResponse(msg: Response) {
-    const uuid = msg.data.uuid;
-    const resolve = this.#parked.get(uuid);
-
-    if (resolve) {
-      this.#parked.delete(uuid);
-      resolve(msg.data.resp);
-    } else {
-      console.warn("Can't find request.");
-    }
-  }
-
-  #handleEventsFromWorker(worker: Worker) {
-    if (IN_NODE) {
-      (worker as unknown as NodeWorker).on('message', (message: Message) => {
-        this.#onMessageFromWorker(worker, message);
-      });
-    } else {
-      worker.onmessage = (ev: MessageEvent) =>
-        this.#onMessageFromWorker(worker, ev.data as Message);
-    }
-  }
-
-  #onMessageFromWorker = async (worker: Worker, message: Message) => {
-    if (!message || !message.type) {
-      return;
-    }
-
-    switch (message.type) {
-      case 'resolve':
-        this.#interruptBuffer = new Int32Array(message.data as SharedArrayBuffer);
-        this.resolve();
-        return;
-
-      case 'response':
-        this.#resolveResponse(message as Response);
-        return;
-
-      default:
-        this.outputQueue.put(message);
-        return;
-
-      case 'sync-request': {
-        const msg = message as SyncRequest;
-        const payload = msg.data.msg;
-        const reqData = msg.data.reqData;
-
-        switch (payload.type) {
-          case 'read': {
-            const response = await this.inputQueue.get();
-            // TODO: Pass a `replacer` function
-            await syncResponse(worker, reqData, response);
-            break;
-          }
-          default:
-            throw new TypeError(`Unsupported request type '${payload.type}'.`);
-        }
-        return;
-      }
-      case 'request':
-        throw new TypeError(
-          "Can't send messages of type 'request' from a worker. Please Use 'sync-request' instead."
-        );
-    }
-  };
+  close(): void;
+  read(): Promise<Message>;
+  flush(): Promise<Message[]>;
+  write(msg: Message): void;
+  request(msg: Message, transferables?: [Transferable]): Promise<any>;
+  interrupt(): void;
 }
 
-// Worker --------------------------------------------------------------
+export interface ChannelWorker {
+  resolve(): void;
+  write(msg: Message, transfer?: [Transferable]): void;
+  read(): Message;
+  handleInterrupt(): void;
+  setInterrupt(interrupt: () => void): void;
+  run(args: string[]): void;
+  inputOrDispatch: () => number;
+  setDispatchHandler: (dispatch: (msg: Message) => void) => void;
+}
 
-import { SyncTask, interruptBuffer } from './task-worker';
+export enum ChannelType {
+  SharedArrayBuffer,
+  ServiceWorker,
+  PostMessage,
+}
 
-export class ChannelWorker {
-  #ep: Endpoint;
-
-  constructor() {
-    this.#ep = (IN_NODE ? require('worker_threads').parentPort : globalThis) as Endpoint;
+export function newChannelMain(url: string, data: Required<WebROptions>) {
+  if (IN_NODE || crossOriginIsolated) {
+    return new SharedBufferChannelMain(url, data);
   }
+  throw new Error('Unable to initialise main thread communication channel');
+}
 
-  resolve() {
-    this.write({ type: 'resolve', data: interruptBuffer.buffer });
-  }
-
-  write(msg: Message, transfer?: [Transferable]) {
-    this.#ep.postMessage(msg, transfer);
-  }
-
-  read(): Message {
-    const msg = { type: 'read' } as Message;
-    const task = new SyncTask(this.#ep, msg);
-    return task.syncify() as Message;
+export function newChannelWorker(channelType: ChannelType) {
+  switch (channelType) {
+    case ChannelType.SharedArrayBuffer:
+      return new SharedBufferChannelWorker();
+    default:
+      throw new Error('Unknown worker channel type recieved');
   }
 }
