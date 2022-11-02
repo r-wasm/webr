04e87e08ea7b16b8b164d3009b3f7648c28e6e03
George Stagg <george.stagg@rstudio.com>
2022-11-02 10:49:10 +0000

    Add unit tests for new R object from main thread

--- src/tests/webR/webr-main.test.ts
+++ src/tests/webR/webr-main.test.ts
@@ -1,6 +1,16 @@
 import { WebR } from '../../webR/webr-main';
 import { Message } from '../../webR/chan/message';
-import { RObjectTree, RawType, RDouble } from '../../webR/robj';
+import {
+  RObjectTree,
+  RawType,
+  RDouble,
+  RLogical,
+  RType,
+  RCharacter,
+  RComplex,
+  RList,
+  REnvironment,
+} from '../../webR/robj';
 
 const webR = new WebR({
   WEBR_URL: '../dist/',
@@ -108,10 +118,126 @@ describe('Evaluate R code', () => {
   });
 });
 
-test('Create simple R object from the main thread', async () => {
-  const jsObj = [1, 2, 3, 6, 11, 23, 47, 106, 235];
-  const rObj = (await webR.newRObject(jsObj)) as RDouble;
-  expect(Array.from(await rObj.toArray())).toEqual(jsObj);
+describe('Create R objects from the main thread', () => {
+  test('Create a logical atomic vector', async () => {
+    const jsObj = [true, false, true, null];
+    const rObj = (await webR.newRObject(jsObj)) as RLogical;
+    expect(await rObj.type()).toEqual(RType.Logical);
+    expect(await rObj.toJs()).toEqual(
+      expect.objectContaining({
+        values: [true, false, true, null],
+        names: null,
+        missing: [false, false, false, true],
+      })
+    );
+  });
+
+  test('Create a double atomic vector', async () => {
+    const jsObj = [1, 2, 3, 6, 11, 23, 47, 106, null];
+    const rObj = (await webR.newRObject(jsObj)) as RDouble;
+    expect(await rObj.type()).toEqual(RType.Double);
+    expect(await rObj.toArray()).toEqual(jsObj);
+  });
+
+  test('Create a character atomic vector', async () => {
+    const jsObj = ['a', 'b', 'c', null];
+    const rObj = (await webR.newRObject(jsObj)) as RCharacter;
+    expect(await rObj.type()).toEqual(RType.Character);
+    expect(await rObj.toJs()).toEqual(
+      expect.objectContaining({
+        values: ['a', 'b', 'c', null],
+        names: null,
+        missing: [false, false, false, true],
+      })
+    );
+  });
+
+  test('Create a complex atomic vector', async () => {
+    const jsObj = [{ re: 1, im: 2 }, { re: -3, im: -4 }, null];
+    const rObj = (await webR.newRObject(jsObj)) as RComplex;
+    expect(await rObj.type()).toEqual(RType.Complex);
+    expect(await rObj.toJs()).toEqual(
+      expect.objectContaining({
+        values: [{ re: 1, im: 2 }, { re: -3, im: -4 }, null],
+        names: null,
+        missing: [false, false, true],
+      })
+    );
+  });
+
+  test('Create a list', async () => {
+    const jsObj = [true, 3.14, 'abc'];
+    const rObj = (await webR.newRObject(jsObj)) as RList;
+    expect(await rObj.type()).toEqual(RType.List);
+    const list = await rObj.toJs();
+    expect(list.values[0]).toEqual({
+      type: 'Logical',
+      names: null,
+      values: [true],
+      missing: [false],
+    });
+    expect(list.values[1]).toEqual({
+      type: 'Double',
+      names: null,
+      values: [3.14],
+      missing: [false],
+    });
+    expect(list.values[2]).toEqual({
+      type: 'Character',
+      names: null,
+      values: ['abc'],
+      missing: [false],
+    });
+  });
+
+  test('Create an environment', async () => {
+    const jsObj = { x: 123, y: 'abc' };
+    const rObj = (await webR.newRObject(jsObj)) as REnvironment;
+    expect(await rObj.type()).toEqual(RType.Environment);
+    expect(await rObj.ls()).toEqual(['x', 'y']);
+    const x = await rObj.get('x');
+    const y = await rObj.get('y');
+    expect(await x.toJs()).toEqual(
+      expect.objectContaining({
+        values: [123],
+        names: null,
+      })
+    );
+    expect(await y.toJs()).toEqual(
+      expect.objectContaining({
+        values: ['abc'],
+        names: null,
+      })
+    );
+  });
+
+  test('Round trip convert nested R objects and ensure result is identical', async () => {
+    const rObj = (
+      await webR.evalRCode(
+        'list(a=list(e=c(T,F,NA),f=c(1,2,3)), b=pairlist(g=c(4L,5L,6L)), c=list(h=c("abc","def"), i=list(7i)))'
+      )
+    ).result as RList;
+    const jsObj = await rObj.toTree();
+    const newRObj = (await webR.newRObject(jsObj)) as RList;
+    const env = (await webR.newRObject({ newRObj, rObj })) as REnvironment;
+    const identical = (await webR.evalRCode('identical(rObj, newRObj)', env)).result as RLogical;
+    expect(await rObj.type()).toEqual(RType.List);
+    expect(await identical.toLogical()).toEqual(true);
+  });
+
+  test('Round trip convert R object containing proxies and ensure result is identical', async () => {
+    const rObj = (
+      await webR.evalRCode(
+        'list(a=list(e=c(T,F,NA),f=c(1,2,3)), b=pairlist(g=c(4L,5L,6L)), c=list(h=c("abc","def"), i=list(7i)))'
+      )
+    ).result as RList;
+    const jsObj = await rObj.toTree({ depth: 1 });
+    const newRObj = (await webR.newRObject(jsObj)) as RList;
+    const env = (await webR.newRObject({ newRObj, rObj })) as REnvironment;
+    const identical = (await webR.evalRCode('identical(rObj, newRObj)', env)).result as RLogical;
+    expect(await rObj.type()).toEqual(RType.List);
+    expect(await identical.toLogical()).toEqual(true);
+  });
 });
 
 afterAll(() => {
