a7684c6aa3501adebaba80856359c11812225f9b
George Stagg <george.stagg@rstudio.com>
2023-03-03 09:08:59 +0000

    Write emfc script to host/bin directory
    This fixes make's prerequisite chain when running `make` after
    `make clean-webr`.
    
    Also updates GHA for the updated emfc path

--- tools/fortran-flang.mk
+++ tools/fortran-flang.mk
@@ -1,5 +1,5 @@
 EMFC_DIR = $(WEBR_ROOT)/tools/flang
-EMFC = $(EMFC_DIR)/emfc
+EMFC = $(WEBR_ROOT)/host/bin/emfc
 
 FORTRAN_WASM_LIB = $(WASM)/lib/libFortranRuntime.a
 FORTRAN_WASM_LDADD = -L$(WASM)/lib -lFortranRuntime
