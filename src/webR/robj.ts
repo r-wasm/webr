38a9bc2e47e0e750705a8b859669b456fbd0d9ac
George Stagg <george.stagg@posit.co>
2023-03-07 15:04:54 +0000

    Rename `toTree()` to `toJs()` (#160)
    * Rename toTree() to toJs()
    * Remove redundant toJs() definition
    * Rename ToTreeOptions to ToJsOptions
    * Merge tree.ts and robj.ts modules
    * Update links in documentation for toJs() changes

--- src/webR/robj.ts
+++ src/webR/robj.ts
@@ -2,7 +2,6 @@
  * Common module for working with R objects.
  * @module RObject
  */
-import { WebRDataTree, WebRDataTreeAtomic } from './tree';
 import * as RMain from './robj-main';
 import * as RWorker from './robj-worker';
 
@@ -76,7 +75,7 @@ export type WebRData =
   | RWorker.RObjectBase
   | RWorker.RObject
   | WebRDataRaw
-  | WebRDataTree
+  | WebRDataJs
   | WebRData[]
   | { [key: string]: WebRData };
 
@@ -89,8 +88,61 @@ export type WebRData =
 export type WebRDataAtomic<T> =
   | WebRDataScalar<T>
   | (T | null)[]
-  | WebRDataTreeAtomic<T>
+  | WebRDataJsAtomic<T>
   | NamedObject<T | null>;
+
+/**
+ * `WebRDataJs` objects form a tree structure, used when serialising R objects
+ * into a JavaScript respresentation.
+ *
+ * Nested R objects are serialised using the {@link WebRDataJsNode} type,
+ * forming branches in the resulting tree structure, with leaves formed by the
+ * remaining types.
+ */
+export type WebRDataJs =
+  | WebRDataJsNull
+  | WebRDataJsString
+  | WebRDataJsSymbol
+  | WebRDataJsNode
+  | WebRDataJsAtomic<RWorker.atomicType>;
+
+export type WebRDataJsNull = {
+  type: 'null';
+};
+
+export type WebRDataJsString = {
+  type: 'string';
+  value: string;
+};
+
+export type WebRDataJsSymbol = {
+  type: 'symbol';
+  printname: string | null;
+  symvalue: RPtr | null;
+  internal: RPtr | null;
+};
+
+export type WebRDataJsNode = {
+  type: 'list' | 'pairlist' | 'environment';
+  names: (string | null)[] | null;
+  values: (WebRDataRaw | RWorker.RObject | RMain.RObject | WebRDataJs)[];
+};
+
+export type WebRDataJsAtomic<T> = {
+  type: 'logical' | 'integer' | 'double' | 'complex' | 'character' | 'raw';
+  names: (string | null)[] | null;
+  values: (T | null)[];
+};
+
+/**
+ * Test for a {@link WebRDataJs} instance.
+ *
+ * @param {any} value The object to test.
+ * @returns {boolean} True if the object is an instance of a {@link WebRDataJs}.
+ */
+export function isWebRDataJs(value: any): value is WebRDataJs {
+  return value && typeof value === 'object' && Object.keys(RTypeMap).includes(value.type as string);
+}
 
 /**
  * A subset of WebRData for scalar JavaScript objects.
