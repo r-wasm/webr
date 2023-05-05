7b511cf3394dbf54941f54b5e7d7dfb55685529c
George Stagg <george.stagg@posit.co>
2023-05-05 11:37:58 +0100

    Add optimisation flag `-Oz` to webr package build
    Also adds some fixes from webr-repo,
     * Add -L$(WEBR_ROOT)/wasm/lib to LDFLAGS
     * Add -DRCPP_DEMANGLER_ENABLED fix

--- packages/webr-vars.mk
+++ packages/webr-vars.mk
@@ -1,7 +1,13 @@
 include ${WASM_TOOLS}/fortran.mk
 -include ~/.webr-vars.mk
 
-R_INCLUDES = -I${R_SOURCE}/build-stage1/include/ -I${R_SOURCE}/src/include
+WEBR_ROOT = $(abspath ..)
+WASM_OPT ?= -Oz
+WEBR_HOST_INCLUDES = -I$(R_SOURCE)/build-stage1/include -I$(R_SOURCE)/src/include
+WEBR_LDFLAGS = -L$(WEBR_ROOT)/wasm/lib
+
+EM_CXX_FIXES = -DRCPP_DEMANGLER_ENABLED=0 -D__STRICT_ANSI__
+EM_CFLAGS = $(WEBR_HOST_INCLUDES) $(WASM_OPT)
 
 CXX98 = em++
 CXX11 = em++
@@ -10,14 +16,14 @@ CXX17 = em++
 CXX20 = em++
 CC = emcc
 CXX = em++
-CFLAGS = -std=gnu11 $(R_INCLUDES)
-CXXFLAGS = -std=gnu++11 -D__STRICT_ANSI__ $(R_INCLUDES)
-CXX98FLAGS = -std=gnu++98 -D__STRICT_ANSI__ $(R_INCLUDES)
-CXX11FLAGS = -std=gnu++11 -D__STRICT_ANSI__ $(R_INCLUDES)
-CXX14FLAGS = -std=gnu++14 -D__STRICT_ANSI__ $(R_INCLUDES)
-CXX17FLAGS = -std=gnu++17 -D__STRICT_ANSI__ $(R_INCLUDES)
-CXX20FLAGS = -std=gnu++20 -D__STRICT_ANSI__ $(R_INCLUDES)
-LDFLAGS = -s SIDE_MODULE=1 -s WASM_BIGINT -s ASSERTIONS=1
+CFLAGS = -std=gnu11 $(EM_CFLAGS)
+CXXFLAGS = -std=gnu++11 $(EM_CXX_FIXES) $(EM_CFLAGS)
+CXX98FLAGS = -std=gnu++98 $(EM_CXX_FIXES) $(EM_CFLAGS)
+CXX11FLAGS = -std=gnu++11 $(EM_CXX_FIXES) $(EM_CFLAGS)
+CXX14FLAGS = -std=gnu++14 $(EM_CXX_FIXES) $(EM_CFLAGS)
+CXX17FLAGS = -std=gnu++17 $(EM_CXX_FIXES) $(EM_CFLAGS)
+CXX20FLAGS = -std=gnu++20 $(EM_CXX_FIXES) $(EM_CFLAGS)
+LDFLAGS = -s SIDE_MODULE=1 -s WASM_BIGINT -s ASSERTIONS=1 $(WEBR_LDFLAGS) $(WASM_OPT)
 FC = emfc
 FLIBS = $(FORTRAN_WASM_LDADD)
 AR = emar
