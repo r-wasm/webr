848346efadb1201f5ef52d948d391c1657dff577
George Stagg <george.stagg@posit.co>
2023-05-25 12:41:36 +0100

    Run fc-cache as part of the Wasm fontconfig build
    By caching information about fonts in `/usr/share/fonts` at build time, webR
    does not need to scan the directory at runtime to generate a list of fonts.
    When using a lazy filesystem, this avoids downloading unused fonts
    unnecessarily.
    
    Font config's cache is stored at `/var/cache/fontconfig` in the virtual
    filesystem.
    
    For this to work well fontconfig must be patched so that,
     * `fc-config` is able to run on the host system through node to generate
       cache files for use in Wasm.
     * The cache files are accepted as valid, even though the timestamp of
       the `/usr/share/fonts` directory in the virtual file system has changed.

--- libs/recipes/fontconfig/rules.mk
+++ libs/recipes/fontconfig/rules.mk
@@ -10,15 +10,21 @@ $(FC_TARBALL):
 	wget -q -O $@ $(FC_URL)
 
 $(FC_DEPS): $(FC_TARBALL) $(LIBXML2_WASM_LIB) $(EM_PKG_CONFIG_PATH)/freetype2.pc
+	rm -rf $(BUILD)/fontconfig-$(FC_VERSION)
 	mkdir -p $(BUILD)/fontconfig-$(FC_VERSION)/build
 	tar -C $(BUILD) -xf $(FC_TARBALL) --exclude=fcobjshash.h
-	cd $(BUILD)/fontconfig-$(FC_VERSION)/build && \
+	cp -r "$(WEBR_ROOT)/patches/fontconfig-$(FC_VERSION)/." \
+	  "$(BUILD)/fontconfig-$(FC_VERSION)/patches"
+	cd $(BUILD)/fontconfig-$(FC_VERSION)/build && quilt push -a && \
 	  LDFLAGS="$(LDFLAGS) -sUSE_FREETYPE=1 -sUSE_PTHREADS=0" \
 	  PTHREAD_CFLAGS=" " \
 	  emconfigure ../configure \
 	    ac_cv_func_fstatfs=no \
+	    ac_cv_func_link=no \
 	    --enable-shared=no \
 	    --enable-static=yes \
 	    --enable-libxml2 \
 	    --prefix=$(WASM) && \
 	  emmake make RUN_FC_CACHE_TEST=false install
+	cd $(BUILD)/fontconfig-$(FC_VERSION)/build/fc-cache && \
+	  make clean && make AM_LDFLAGS="-s NODERAWFS=1"
