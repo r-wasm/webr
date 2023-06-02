bb0ec976f126b4b784dfb9f9ae16482448a8d5ba
George Stagg <george.stagg@posit.co>
2023-06-02 13:48:53 +0100

    Fix the expat cmake helpers for static lib only

--- libs/recipes/expat/rules.mk
+++ libs/recipes/expat/rules.mk
@@ -9,10 +9,18 @@ $(EXPAT_TARBALL):
 	mkdir -p $(DOWNLOAD)
 	wget $(EXPAT_URL) -O $@
 
+# Expat installs cmake helpers that include a check to ensure the expat shared
+# library exists on disk. Here, however, we're only building static libs, so we
+# patch out the check before installing expat. Without this any build using
+# cmake fails after installation, even if expat isn't required.
 $(EXPAT_WASM_LIB): $(EXPAT_TARBALL)
 	mkdir -p $(BUILD)/expat-$(EXPAT_VERSION)/build
 	tar -C $(BUILD) -xf $(EXPAT_TARBALL)
 	cd $(BUILD)/expat-$(EXPAT_VERSION)/build && \
+	  sed -i.bak 's/^list(APPEND _cmake_import_check_files_for_expat.*//' \
+	    ../cmake/autotools/expat-noconfig__linux.cmake.in && \
+	  sed -i.bak 's/^list(APPEND _cmake_import_check_files_for_expat.*//' \
+	    ../cmake/autotools/expat-noconfig__macos.cmake.in && \
 	  emconfigure ../configure \
 	    --enable-shared=no \
 	    --enable-static=yes \
