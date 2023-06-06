518c6a4ff4da9cfd310dd0ddeb62647e1224c31b
George Stagg <george.stagg@posit.co>
2023-06-06 14:40:07 +0100

    Update esbuild.ts for new esbuild context API
    `make serve` is now implemented using esbuild's .serve() and .watch()
    API.
    
    `console.ts` is no longer an esbuild entrypoint, as it is now exposed
    directly on the default `webR` entrypoint.

--- src/esbuild.ts
+++ src/esbuild.ts
@@ -1,88 +1,110 @@
-const esbuild = require('esbuild');
-const cssModulesPlugin = require('esbuild-css-modules-plugin');
+import esbuild from 'esbuild';
+import cssModulesPlugin from 'esbuild-css-modules-plugin';
+import http from 'http';
 
-function build({ input, output, platform, minify }) {
-  esbuild.build({
-    entryPoints: [`${input}`],
-    bundle: true,
-    target: ['es2020','node12'],
-    minify: minify,
-    sourcemap: true,
-    outfile: output,
-    logLevel: 'info',
-    platform: platform,
-    mainFields: ['main', 'module'],
-    external: ['worker_threads', 'path', 'fs'],
+let serve = false;
+let prod = false;
+
+if (process.argv.some((x) => x === '--serve')) {
+  serve = true;
+}
+
+if (process.argv.some((x) => x === '--prod')) {
+  prod = true;
+}
+
+function build(input: string, output: string, platform: esbuild.Platform) {
+  return esbuild.context({
     assetNames: 'assets/[name]-[hash]',
+    bundle: true,
+    entryPoints: [ input ],
+    external: ['worker_threads', 'path', 'fs'],
     loader: {
       '.jpg': 'file',
       '.png': 'file',
       '.gif': 'file',
     },
+    mainFields: ['main', 'module'],
+    minify: prod,
+    outfile: output,
+    platform: platform,
     plugins: [
-    	cssModulesPlugin({
-    		inject: (cssContent, digest) => `console.log("${cssContent}", "${digest}")`,
-    	})
+      cssModulesPlugin({
+        inject: (cssContent, digest) => `console.log("${cssContent}", "${digest}")`,
+      })
     ],
-  }).catch(() => process.exit(1));
+    sourcemap: true,
+    target: ['es2020', 'node12'],
+  });
 }
 
 const outputs = {
-  'console.mjs': {
-    input: "console/console.ts",
-    platform: 'neutral',
-    minify: true,
-  },
-  'repl.mjs': {
-    input: "repl/repl.ts",
-    platform: 'neutral',
-    minify: true,
-  },
-  'webr-worker.js': {
-    input: "webR/webr-worker.ts",
-    platform: 'node',
-    minify: false,
-  },
-  'webr-serviceworker.js': {
-    input: "webR/chan/serviceworker.ts",
-    platform: 'browser',
-    minify: false,
-  },
-  'webr-serviceworker.mjs': {
-    input: "webR/chan/serviceworker.ts",
-    platform: 'neutral',
-    minify: false,
-  },
-  'webr.mjs': {
-    input: "webR/webr-main.ts",
-    platform: 'neutral',
-    minify: true,
-  },
-  'webr.cjs': {
-    input: "webR/webr-main.ts",
-    platform: 'node',
-    minify: true,
-  },
+  browser: [
+    build('repl/repl.ts', '../dist/repl.mjs', 'neutral'),
+    build('webR/chan/serviceworker.ts', '../dist/webr-serviceworker.js', 'browser'),
+    build('webR/webr-worker.ts', '../dist/webr-worker.js', 'node'),
+    build('webR/webr-main.ts', '../dist/webr.mjs', 'neutral'),
+  ],
+  npm: [
+    build('webR/chan/serviceworker.ts', './dist/webr-serviceworker.mjs', 'neutral'),
+    build('webR/chan/serviceworker.ts', './dist/webr-serviceworker.js', 'browser'),
+    build('webR/webr-worker.ts', './dist/webr-worker.js', 'node'),
+    build('webR/webr-main.ts', './dist/webr.cjs', 'node'),
+    build('webR/webr-main.ts', './dist/webr.mjs', 'neutral'),
+  ]
 };
+const allOutputs = outputs.browser.concat(outputs.npm);
 
-// Build for web release package
-[
-  'console.mjs',
-  'repl.mjs',
-  'webr-serviceworker.js',
-  'webr-worker.js',
-  'webr.mjs'
-].map( (outfile) => {
-  build(Object.assign(outputs[outfile], { output: `../dist/${outfile}`}))
-});
+allOutputs.forEach((build) => {
+  build
+    .then((context) => {
+      context.rebuild();
+      return context;
+    })
+    .then((context) => {
+      if (serve) context.watch();
+    })
+    .catch((reason) => {
+      console.error(reason);
+      throw new Error('A problem occured building webR distribution with esbuild');
+    });
+  }
+);
 
-// Build for npm release package
-[
-  'webr-serviceworker.mjs',
-  'webr-serviceworker.js',
-  'webr-worker.js',
-  'webr.cjs',
-  'webr.mjs'
-].map( (outfile) => {
-  build(Object.assign(outputs[outfile], { output: `./dist/${outfile}`}))
-});
+if (serve) {
+  outputs.browser[0]
+    .then((context) => {
+      context.serve({ servedir: '../dist', port: 8001 }).then(() => {
+        http
+          .createServer((req, res) => {
+            const { url, method, headers } = req;
+            req.pipe(
+              http.request(
+                { hostname: '127.0.0.1', port: 8001, path: url, method, headers },
+                (proxyRes) => {
+                  const client = res.writeHead(proxyRes.statusCode!, {
+                    ...proxyRes.headers,
+                    'cross-origin-opener-policy': 'same-origin',
+                    'cross-origin-embedder-policy': 'require-corp',
+                  });
+                  proxyRes.pipe(res, { end: true });
+                }
+              ),
+              { end: true }
+            );
+          })
+          .listen(8000);
+        console.log('Server listening on http://127.0.0.1:8000.');
+      });
+    })
+    .catch((reason) => {
+      console.error(reason);
+      throw new Error('A problem occured service webR distribution with esbuild');
+    });
+} else {
+  allOutputs.forEach(build => {
+    build.then((context) => {
+      context.dispose();
+    });
+  });
+}
