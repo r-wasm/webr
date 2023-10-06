8bdd2e938ad83b56ae5fa399c642efffac7711cb
colin <colin@thinkr.fr>
2023-10-06 21:47:44 +0200

    feat: shim_install() is now exported

--- packages/webr/R/shims.R
+++ packages/webr/R/shims.R
@@ -1,4 +1,14 @@
-#' shim base functions
+#' Shim functions from base R
+#'
+#' @description
+#' Replace base R functions with implementations
+#' that work in the webR environment.
+#'
+#' @details
+#' The shimmed functions are:
+#' - install.packages() - replaced by webr::install().
+#'
+#' @export
 shim_install <- function(){
   .e <- new.env()
   .e[["install.packages"]] <- webr::install
