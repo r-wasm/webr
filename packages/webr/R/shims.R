faf473951956e48ad2cb90640758df1c9d6e5da7
George Stagg <george.stagg@posit.co>
2023-10-09 12:48:01 +0100

    Remove attach hook in webr::shim_install()

--- packages/webr/R/shims.R
+++ packages/webr/R/shims.R
@@ -12,8 +12,6 @@
 shim_install <- function(){
   .e <- new.env()
   .e[["install.packages"]] <- webr::install
-  setHook(packageEvent("utils", "attach"), function(...) {
-    attach(.e, name = "webr_shims", warn.conflicts = FALSE)
-  })
+  attach(.e, name = "webr_shims", warn.conflicts = FALSE)
 }
 
