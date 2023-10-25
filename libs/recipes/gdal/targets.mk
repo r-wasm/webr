f5d59109fa799af7560262f4eea556819345c272
George Stagg <george.stagg@posit.co>
2023-10-25 15:11:38 +0100

    Set some lazy loaded files to load from image

--- libs/recipes/gdal/targets.mk
+++ libs/recipes/gdal/targets.mk
@@ -1,3 +1,3 @@
 GDAL_WASM_LIB = $(WASM)/lib/libgdal.a
 OPTIONAL_WASM_LIBS += $(GDAL_WASM_LIB)
-WASM_LAZY_VFS += -f "$(WASM)/share/gdal@/usr/share/gdal"
+WASM_LAZY_VFS += -i "$(WASM)/share/gdal@/usr/share/gdal"
