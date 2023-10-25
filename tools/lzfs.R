a8ac1060cc6934037830546116fa93d43094076c
George Stagg <george.stagg@posit.co>
2023-10-25 15:09:47 +0100

    Update lzfs.R tool to allow lazy load from image
    With this change, the Emscripten filesystem API is used along with the
    new webR private API functions `mountImageUrl` and `mountImagePath` so
    that R system files that are to be lazy loaded can be done so by
    mounting an Emscripten filesystem image, rather than downloading each
    file individually.

--- tools/lzfs.R
+++ tools/lzfs.R
@@ -11,6 +11,8 @@ Arguments:
   -d, --dest      If provided, copy source files to destination vfs directory.
   -f path@parent  Add all files under the directory 'path' to the Emscripten
                   filesystem as lazy files, under the directory 'parent'.
+  -i path@parent  Add all files under the directory 'path' to the Emscripten
+                  filesystem as lazy filesystem images.
   -h, --help      Display this help message.
   -o file         If provided, write output to 'file' rather than stdout.
   -u, --URL       If provided, set a prefix for backing URLs.
@@ -37,6 +39,7 @@ out <- stdout()
 url <- "."
 verbose <- FALSE
 path_vec <- character(0)
+image_vec <- character(0)
 
 while (length(args) > 0) {
     switch(args[1],
@@ -66,6 +69,10 @@ while (length(args) > 0) {
             path_vec <- append(path_vec, args[2])
             args <- tail(args, -2)
         },
+        `-i` = {
+            image_vec <- append(image_vec, args[2])
+            args <- tail(args, -2)
+        },
         {
             message(paste("Unrecognised argument:", args[1]))
             usage()
@@ -73,14 +80,68 @@ while (length(args) > 0) {
     )
 }
 
-# TODO: Check if VFS dir exists without relying on a FS.stat exception
-mkdir_tmpl <- "try{FS.stat('%s')}catch{FS.mkdirTree('%s')}\n"
 file_tmpl <- "loadFile('/%s','%s','%s');\n"
+image_tmpl <- "loadImage('/%s','%s','%s','%s');\n"
 header_out <- r"(var Module = globalThis.Module;
 Module.createLazyFilesystem = function () {
-var loadFile = function(dir, file, src) {
+let fsPathCache = [];
+
+// Load a single file into the VFS. Lazy load in browser, preload in Node
+let loadFile = function(dir, file, src) {
+  if (fsPathCache.includes(`${dir}/${file}`)) {
+    return;
+  }
+  fsPathCache.push(`${dir}/${file}`);
+
   let fn = ENVIRONMENT_IS_NODE ? FS.createPreloadedFile : FS.createLazyFile;
+  FS.mkdirTree(dir);
   fn(dir, file, Module.locateFile(src), true, true);
+}
+
+// Lazy load a file from an Emscripten FS image
+let loadImage = function(dir, file, src, mountpoint) {
+  if (fsPathCache.includes(`${dir}/${file}`)) {
+    return;
+  }
+  fsPathCache.push(`${dir}/${file}`);
+
+  FS.mkdirTree(dir);
+  let node = FS.createFile(dir, file, { isDevice: false } , true, true);
+
+  let forceLoadFile = function(obj) {
+    if (obj.isDevice || obj.isFolder || obj.link || obj.contents) {
+      return true;
+    }
+
+    if (ENVIRONMENT_IS_NODE) {
+      Module.mountImagePath(Module.locateFile(src), mountpoint)
+    } else {
+      Module.mountImageUrl(Module.locateFile(src), mountpoint)
+    }
+
+    let mountNode = Module.FS.lookupPath(`${dir}/${file}`).node;
+    node.contents = mountNode.contents;
+    node.size = mountNode.size;
+  }
+
+  Object.defineProperties(node, {
+    usedBytes: {
+      get: function() {
+        forceLoadFile(node);
+        return node.size;
+      }
+    }
+  });
+
+  let stream_ops = {};
+  Object.keys(node.stream_ops).forEach((key) => {
+    let fn = node.stream_ops[key];
+    stream_ops[key] = (...args) => {
+      forceLoadFile(node);
+      return fn(...args);
+    };
+  });
+  node.stream_ops = stream_ops;
 }
 )"
 dirs_out <- ""
@@ -97,7 +158,6 @@ for (paths in strsplit(path_vec, "@")) {
         # Create directory structure
         dest_dir <- gsub("/$", "", paths[2])
         new_dirs <- file.path(dest_dir, list_dirs(src_path, full = FALSE))
-        dirs_out <- append(dirs_out, sprintf(mkdir_tmpl, new_dirs, new_dirs))
 
         # Create lazy files
         files <- file.path(dest_dir,
@@ -125,4 +185,53 @@ for (paths in strsplit(path_vec, "@")) {
     }
 }
 
-writeLines(c(header_out, dirs_out, files_out, footer_out), sep = "", con = out)
+for (paths in strsplit(image_vec, "@")) {
+    if (!file.exists(paths[1])) {
+        next
+    }
+    if (verbose) message(paste("Adding", paths[1], "(as image)"))
+
+    for (src_path in paths[1]) {
+        # Create lazy files
+        dest_dir <- gsub("/$", "", paths[2])
+        files <- file.path(dest_dir,
+            list_files(src_path, full = FALSE, recursive = TRUE)
+        )
+        parents <- gsub("^/", "", dirname(files))
+        files <- basename(files)
+        data_file <- paste0(gsub("^/", "", dest_dir), '.data')
+        js_file <- paste0(gsub("^/", "", dest_dir), '.js')
+        data_url <- file.path(gsub("/$", "", url), data_file)
+
+        files_out <- append(files_out,
+            sprintf(image_tmpl, parents, files, data_url, paths[2])
+        )
+
+        # Build image from source files
+        dir.create(file.path(dest, dirname(js_file)),
+            showWarnings = FALSE, recursive = TRUE)
+        emscripten_root <- dirname(Sys.which("emcc")[[1]])
+        file_packager <- file.path(emscripten_root, "tools", "file_packager")
+        res <- system2(file_packager,
+            args = c(file.path(dest, data_file),
+                "--preload", sprintf("'%s@/'", src_path),
+                "--separate-metadata",
+                sprintf("--js-output='%s'", file.path(dest, js_file))
+            ),
+            stdout = TRUE,
+            stderr = TRUE
+        )
+        unlink(file.path(dest, js_file))
+
+        # If there is some problem, print the output of `file_packager` and stop
+        status <- attr(res, "status")
+        if (!is.null(status) && status != 0) {
+            stop(
+                "An error occurred running `file_packager`:\n",
+                paste(res, collapse = "\n")
+            )
+        }
+    }
+}
+
+writeLines(c(header_out, files_out, footer_out), sep = "", con = out)
