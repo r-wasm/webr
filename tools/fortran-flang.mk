cbb25dbd7113221ffb242c605c43dbed82ec756b
George Stagg <george.stagg@posit.co>
2023-11-27 15:07:54 +0000

    Minor tweaks to Makefiles

--- tools/fortran-flang.mk
+++ tools/fortran-flang.mk
@@ -3,14 +3,16 @@ EMFC = $(HOST)/bin/flang-new
 FORTRAN_WASM_LIB = $(WASM)/lib/libFortranRuntime.a
 FORTRAN_WASM_LDADD = -L$(WASM)/lib -lFortranRuntime
 
-.PHONY: flang-submodule
-flang-submodule:
-	git submodule update --init
-
-$(EMFC) $(FORTRAN_WASM_LIB): flang-submodule
+$(EMFC) $(FORTRAN_WASM_LIB): $(TOOLS)/flang/Makefile
 	cd $(TOOLS)/flang && \
 	  $(MAKE) PREFIX=$(WEBR_ROOT) && \
 	  $(MAKE) PREFIX=$(WEBR_ROOT) install
+
+$(TOOLS)/flang/Makefile:
+	git submodule update --init
+
+.PHONY: flang-submodule
+flang-submodule: $(TOOLS)/flang/Makefile
 
 .PHONY: clean-tools
 clean-tools:
