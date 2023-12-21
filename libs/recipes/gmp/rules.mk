0db331d80d5ef88ac05ef56db5d96d0436b22797
George Stagg <george.stagg@posit.co>
2023-12-21 16:59:08 +0000

    Fix gmp cross-compile build on macOS

--- libs/recipes/gmp/rules.mk
+++ libs/recipes/gmp/rules.mk
@@ -2,6 +2,13 @@ GMP_VERSION = 6.3.0
 GMP_TARBALL = $(DOWNLOAD)/gmp-$(GMP_VERSION).tar.gz
 GMP_URL = https://ftp.gnu.org/gnu/gmp/gmp-$(GMP_VERSION).tar.xz
 
+# Cross-compile fix for Emscripten-on-macOS
+UNAME := $(shell uname)
+ifeq ($(UNAME),Darwin)
+	HOST_CFLAGS += "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+	HOST_CFLAGS += "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
+endif
+
 .PHONY: gmp
 gmp: $(GMP_WASM_LIB)
 
@@ -13,13 +20,14 @@ $(GMP_WASM_LIB): $(GMP_TARBALL)
 	mkdir -p $(BUILD)/gmp-$(GMP_VERSION)/build
 	tar -C $(BUILD) -xf $(GMP_TARBALL)
 	cd $(BUILD)/gmp-$(GMP_VERSION)/build && \
-	  emconfigure ../configure \
-	    --build=x86_64-pc-linux-gnu \
-	    --host=wasm32-unknown-emscripten \
-	    --enable-shared=no \
-	    --enable-static=yes \
-	    --disable-assembly \
-	    --enable-cxx \
-	    --prefix=$(WASM) && \
+	    emconfigure bash -c '\
+	      HOST_CC="$${HOST_CC} $(HOST_CFLAGS)" ../configure \
+	      --host=wasm32-unknown-emscripten \
+	      --enable-shared=no \
+	      --enable-static=yes \
+	      --disable-assembly \
+	      --enable-cxx \
+	      --prefix=$(WASM) \
+	    ' && \
 	  emmake make install
 
