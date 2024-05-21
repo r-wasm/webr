bb37ef3b57d17a32f407d70cafb59a6022363dfe
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-05-21 23:50:06 +0300

    Add headers to vfs

--- libs/recipes/oneTBB/targets.mk
+++ libs/recipes/oneTBB/targets.mk
@@ -1,2 +1,4 @@
 ONETBB_WASM_LIB = $(WASM)/lib/libtbb.a $(WASM)/lib/libtbbmalloc.a
 OPTIONAL_WASM_LIBS += $(ONETBB_WASM_LIB)
+WASM_LAZY_VFS += -i "$(WASM)/include/tbb@/usr/include/tbb"
+WASM_LAZY_VFS += -i "$(WASM)/include/oneapi@/usr/include/oneapi"
