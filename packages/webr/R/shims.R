fa8ad704682f9323c09704156da4642da70e22ad
Tony Boyles <AABoyles@gmail.com>
2025-09-23 07:54:08 -0400

    Added requireNamespace shim (#570)
    Fixes #474

--- packages/webr/R/shims.R
+++ packages/webr/R/shims.R
@@ -9,12 +9,14 @@
 #' - install.packages() - replaced by webr::install().
 #' - library() - replaced by webr::library_shim().
 #' - require() - replaced by webr::require_shim().
+#' - requireNamespace() - replaced by webr::requireNamespace_shim(). 
 #'
 #' @export
 shim_install <- function() {
   .e <- new.env()
   .e[["install.packages"]] <- webr::install
   .e[["library"]] <- webr::library_shim
+  .e[["requireNamespace"]] <- webr::requireNamespace_shim
   .e[["require"]] <- webr::require_shim
   attach(.e, name = "webr_shims", warn.conflicts = FALSE)
 }
